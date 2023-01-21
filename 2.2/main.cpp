107394ac57249a9edb4ba7a79502fcc0cca6a740
Dmitriy Borisov <113330319+DmitriyCherep@users.noreply.github.com>
2023-01-21 18:25:26 +0300

    Update main.cpp

--- 2.2/main.cpp
+++ 2.2/main.cpp
@@ -1,38 +1,50 @@
 #include <iostream>
-#include <cctype>
-#include "modAlphaCipher.h"
-#include <locale>
-using namespace std;
-bool isValid(const wstring& s)
+#include <limits>
+#include "headers/routeCipher.h"
+bool isValid(const std::string& s)
 {
-    for(auto c:s)
-        if (!iswalpha(c) || !iswupper(c))
-            return false;
-    return true;
+ for(unsigned i = 0; i < s.size(); i++) {
+ if (!((int)s[i] > 31 && (int)s[i] < 128))
+ return false;
+ }
+ return true;
 }
-int main(int argc, char **argv)
+int main()
 {
-    int klych=2;
-    locale loc("ru_RU.UTF-8");
-    locale::global(loc);
-    wstring z;
-    wstring text;
-    unsigned op;
-    Cipher cipher(klych);
-    do {
-        wcout<<L"Какую операцию необходимо произветси(0-exit, 1-encrypt, 2-decrypt): ";
-        wcin>>op;
-        if (isValid(text)) {
-            if (op >0) {
-            wcout<<L"Введите текст: ";
-            wcin>>text;
-                if (op==1) {
-                    wcout<<L"Закодированный текст: "<<cipher.zakodirovatCipher(cipher, text)<<endl;
-                } else {
-                    wcout<<L"Раскодированный текст: "<<cipher.raskodirovatCipher(cipher, text)<<endl;
-                }
-                }
-        }
-    } while (op!=0);
-    return 0;
+ int key;
+ std::string text;
+ unsigned op;
+ std::cout << "Key: ";
+ std::cin >> key;
+ while(std::cin.fail()) {
+ std::cout << "Can only receive integers. Key: ";
+ std::cin.clear();
+ std::cin.ignore(256,'\n');
+ std::cin >> key;
+ }
+ routeCipher cipher(key);
+ do {
+ std::cout<<"Operation (0-exit, 1-encrypt, 2-decrypt): ";
+ std::cin>>op;
+ if (op > 2) {
+ std::cout<<"Illegal operation\n";
+ } else if (op >0) {
+ std::cout<<"Text: ";
+ std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\
+n');
+ std::getline(std::cin, text);
+ if (isValid(text)) {
+ if (op==1) {
+ std::cout<< "Encrypted text: " << cipher.encrypt(text)
+<<std::endl;
+ } else {
+ std::cout<< "Decrypted text: " << cipher.decrypt(text)
+<<std::endl;
+ }
+ } else {
+ std::cout<<"Operation aborted: invalid text\n";
+ }
+ }
+ } while (op!=0);
+ return 0;
 }
