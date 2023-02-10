b97e59c62d59e9967e92c609add493ff13f2cacb
Dmitriy Borisov <113330319+DmitriyCherep@users.noreply.github.com>
2023-02-10 18:34:58 +0300

    Update main.cpp

--- 2.2/main.cpp
+++ 2.2/main.cpp
@@ -1,43 +1,38 @@
-#include "modTableCipher.h"
+#include <iostream>
+#include <cctype>
+#include "modAlphaCipher.h"
+#include <locale>
 using namespace std;
-bool isValid(const string& s)
+bool isValid(const wstring& s)
 {
- for(auto c:s)
- if (!isalpha(c) || !isupper(c))
- return false;
- return true;
+    for(auto c:s)
+        if (!iswalpha(c) || !iswupper(c))
+            return false;
+    return true;
 }
 int main(int argc, char **argv)
 {
- int key;
- string text;
- unsigned op;
- cout<<"Cipher ready. Input key: ";
- cin>>key;
- if (!cin.good()) {
- cerr<<"key not valid\n";
- return 1;
- }
- cout<<"Key loaded\n";
- modTableCipher cipher(key);
- do {
- cout<<"Cipher ready. Input operation (0-exit, 1-encrypt, 2-decrypt): ";
- cin>>op;
- if (op > 2) {
- cout<<"Illegal operation\n";
- } else if (op >0) {
- cout<<"Cipher ready. Input text: ";
- cin>>text;
- if (isValid(text)) {
- if (op==1) {
- cout<<"Encrypted text:"<<cipher.encrypt(text)<<endl;
- } else {
- cout<<"Decrypted text:"<<cipher.decrypt(text)<<endl;
- }
- } else {
- cout<<"Operation aborted: invalid text\n";
- }
- }
- } while (op!=0);
- return 0;
+    int klych=2;
+    locale loc("ru_RU.UTF-8");
+    locale::global(loc);
+    wstring z;
+    wstring text;
+    unsigned op;
+    Cipher cipher(klych);
+    do {
+        wcout<<L"Какую операцию необходимо произветси(0-exit, 1-encrypt, 2-decrypt): ";
+        wcin>>op;
+        if (isValid(text)) {
+            if (op >0) {
+            wcout<<L"Введите текст: ";
+            wcin>>text;
+                if (op==1) {
+                    wcout<<L"Закодированный текст: "<<cipher.zakodirovatCipher(cipher, text)<<endl;
+                } else {
+                    wcout<<L"Раскодированный текст: "<<cipher.raskodirovatCipher(cipher, text)<<endl;
+                }
+                }
+        }
+    } while (op!=0);
+    return 0;
 }
