161bee23fdec1d83ab36125c98df70af66e5fdb7
Dmitriy Borisov <113330319+DmitriyCherep@users.noreply.github.com>
2023-02-14 20:22:25 +0300

    Update main.cpp

--- 2.1/main.cpp
+++ 2.1/main.cpp
@@ -3,7 +3,6 @@
 #include "modAlphaCipher.h"
 #include <locale>
 using namespace std;
-// проверка, чтобы строка состояла только из прописных букв
 bool isValid(const wstring& s)
 {
     for(auto c:s)
@@ -13,37 +12,37 @@ bool isValid(const wstring& s)
 }
 int main(int argc, char **argv)
 {
- locale loc("ru_RU.UTF-8");
+    locale loc("ru_RU.UTF-8");
     locale::global(loc);
     wstring key;
     wstring text;
     unsigned op;
- wcout<<L" Input key: ";
- wcin>>key;
+    wcout<<L"Cipher ready. Input key: ";
+    wcin>>key;
     if (!isValid(key)) {
- wcerr<<L"key not valid\n";
+        wcerr<<L"key not valid\n";
         return 1;
     }
- wcout<<L"Key loaded\n";
- cipher(key);
+    wcout<<L"Key loaded\n";
+    modAlphaCipher cipher(key);
     do {
- wcout<<L" Input operation (0-exit, 1-encrypt, 2-decrypt): ";
- wcin>>op;
+        wcout<<L"Cipher ready. Input operation (0-exit, 1-encrypt, 2-decrypt): ";
+        wcin>>op;
         if (op > 2) {
- wcout<<L"Illegal operation\n";
- } else if (op >0) {
- wcout<<L" Input text: ";
- wcin>>text;
+            wcout<<L"Illegal operation\n";
+        } else if (op >0) {
+            wcout<<L"Cipher ready. Input text: ";
+            wcin>>text;
             if (isValid(text)) {
                 if (op==1) {
- L"Encrypted text: "<<cipher.encrypt(text)<<endl;
- } else {
- L"Decrypted text: "<<cipher.decrypt(text)<<endl;
+                    wcout<<L"Encrypted text: "<<cipher.encrypt(text)<<endl;
+                } else {
+                    wcout<<L"Decrypted text: "<<cipher.decrypt(text)<<endl;
                 }
             } else {
- wcout<<L"Operation aborted: invalid text\n";
+                wcout<<L"Operation aborted: invalid text\n";
             }
         }
- } while (op!=0);
+    } while (op!=0);
     return 0;
 }
